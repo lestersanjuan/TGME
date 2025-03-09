30e42e1e38aeaafb89fc9afe1ed294aca3126b4a
ConnorJLee <104226360+ConnorJLee@users.noreply.github.com>
2025-03-08 21:08:53 -0800

    Add GuiHandler

---
+++ GuiHandler.java
@@ -0,0 +1,12 @@
+
+public abstract class GuiHandler {
+	protected GameEngine gameEngine;
+	
+	abstract void UpdateUI(Board boardState);
+	
+	abstract void AffectGameEngine(String command);
+	
+	public void SetGameEngine(GameEngine gameEngine) {
+		this.gameEngine = gameEngine;
+	}
+}
