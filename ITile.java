f392b4ea016ede9a340a1de42639a8073c4fddfe
ConnorJLee <104226360+ConnorJLee@users.noreply.github.com>
2025-03-11 11:52:33 -0700

    Tile Updates
    Add more functionality to tiles

--- ITile.java
+++ ITile.java
@@ -1,7 +1,11 @@
+import java.util.Map;
 
 public interface ITile {
 	// public List<Integer> GetPosition();
 	// public void SetPosition(List<Integer> coords);
 	public String GetValue();
 	public void SetValue(String value);
+	public String GetData(String key);
+	public void SetData(String key, String value);
+	public void RemoveData(String key);
 }
--- Tile.java
+++ Tile.java
@@ -1,14 +1,37 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class Tile implements ITile{
-	private String value;
+	private String currentValue;
+	private Map<String, String> data;
+	
+	public Tile (String value) {
+		this.currentValue = value;
+		this.data = new HashMap<String, String>();
+	}
 
 	@Override
 	public String GetValue() {
-		return this.value;
+		return this.currentValue;
 	}
 
 	@Override
 	public void SetValue(String value) {
-		this.value = value;
+		this.currentValue = value;
+	}
+
+	@Override
+	public String GetData(String key) {
+		return this.data.get(key);
+	}
+
+	@Override
+	public void SetData(String key, String value) {
+		this.data.put(key, value);
+	}
+
+	@Override
+	public void RemoveData(String key) {
+		this.data.remove(key);
 	}
 }
