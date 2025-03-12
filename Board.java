380f7fdaa1360b45a95e2839066dfabd59ddcfb7
lestersanjuan <lsanjua1@uci.edu>
2025-03-11 20:17:51 -0700

    Added a GetBoard function()
    returns currentBoard object while printing out its current state

--- Board.java
+++ Board.java
@@ -1,18 +1,19 @@
 import java.util.List;
 
 public class Board {
-	private List<List<ITile>> currentBoard;
+	protected List<List<Tile>> currentBoard;
 	
 	private Integer width;
 	private Integer height;
 	
-	public void PlaceTile(ITile tile, Integer row, Integer col) {
+	public void PlaceTile(Tile tile, Integer row, Integer col) {
+		System.out.println("testing");
 		this.currentBoard.get(row).set(col, tile);
 	}
 	
-	public ITile RemoveTile(Integer row, Integer col) {
+	public Tile RemoveTile(Integer row, Integer col) {
 		
-		ITile tileRemoved = this.currentBoard.get(row).get(col);
+		Tile tileRemoved = this.currentBoard.get(row).get(col);
 		this.currentBoard.get(row).remove(col.intValue());
 		return tileRemoved;
 	}
--- TwentyFortyEightBoard.java
+++ TwentyFortyEightBoard.java
@@ -1,6 +1,7 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
-
-
 
 /** 
  * 
@@ -18,22 +19,37 @@ public class TwentyFortyEightBoard extends Board{
      */
 
     public TwentyFortyEightBoard(){
+        System.out.println("test");
         Random random = new Random();
         Integer randomHeight = random.nextInt(1,4);
         Integer randomWidth = random.nextInt(1,4);
         Integer randomStart = random.nextInt(1,2) * 2; //multiply by 2 to keep it a multiple of 2
-
-        for (int i = 0; i < 5; i++){
-            for (int j = 0; j < 5; j++){ //populate currentBoard with all new Tiles
+        this.currentBoard = new ArrayList<>(HEIGHT);
+        for (int i = 0; i < HEIGHT; i++){
+            List<Tile> current = new ArrayList<Tile>(WIDTH);
+            for (int j = 0; j < WIDTH; j++){
                 Tile newTile = new Tile();
-                this.PlaceTile(newTile, i, j);
+                newTile.SetValue("0");
+                current.add(newTile);
             }
+            currentBoard.add(current);
         }
+        System.out.println("????/");
         Tile newTile = new Tile();
         newTile.SetValue(randomStart.toString());
         //Place First Tile
         this.PlaceTile(newTile, randomHeight, randomWidth);
         //Check If there is tile
+    }
+    //Prints out current board
+    public List<List<Tile>> getBoard(){
+        for (int i = 0; i < HEIGHT; i++){
+            for (int j = 0; j < WIDTH; j++){
+                System.out.print(this.currentBoard.get(i).get(j).GetValue() + " ");
+            }
+            System.out.println(" ");
+        }
+        return this.currentBoard;
     }
 
     
