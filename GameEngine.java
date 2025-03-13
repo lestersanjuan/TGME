d38b1f77153ddc431f2b3f89f4852ca51f5b3f0a
lestersanjuan <lsanjua1@uci.edu>
2025-03-12 18:49:02 -0700

    idek what the hell changed

--- GameEngine.java
+++ GameEngine.java
@@ -3,14 +3,16 @@ public abstract class GameEngine {
 	protected Board gameBoard;
 	protected Integer gameId;
 	protected Integer score;
-	
+
 	abstract boolean Action(String command);
-	
+
 	public Integer GetGameId() {
 		return this.gameId;
 	}
-	
+
 	public Board GetBoardState() {
-		return this.gameBoard; //Should return copy if possible
+		return this.gameBoard; // Should return copy if possible
 	}
+
+	abstract void MatchTiles();
 }
--- TwentyFortyEightBoard.java
+++ TwentyFortyEightBoard.java
@@ -3,31 +3,31 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-/** 
+/**
  * 
- * Essentially 2048 game board will be a 4x4 Tile Game board, Where if moved left or moved right or up
- * or down will 
-*/
-    
-public class TwentyFortyEightBoard extends Board implements IInputHandler{
+ * Essentially 2048 game board will be a 4x4 Tile Game board, Where if moved
+ * left or moved right or up
+ * or down will
+ */
+
+public class TwentyFortyEightBoard extends Board {
     private final Integer WIDTH = 4; // Game board does not change sizes
     private final Integer HEIGHT = 4;
-
 
     /**
      * To Begin the Game
      */
 
-    public TwentyFortyEightBoard(){
+    public TwentyFortyEightBoard() {
         System.out.println("test");
         Random random = new Random();
         Integer randomHeight = random.nextInt(4);
         Integer randomWidth = random.nextInt(4);
-        Integer randomStart = random.nextInt(1,2) * 2; //multiply by 2 to keep it a multiple of 2
+        Integer randomStart = random.nextInt(1, 2) * 2; // multiply by 2 to keep it a multiple of 2
         this.currentBoard = new ArrayList<>(HEIGHT);
-        for (int i = 0; i < HEIGHT; i++){
+        for (int i = 0; i < HEIGHT; i++) {
             List<Tile> current = new ArrayList<Tile>(WIDTH);
-            for (int j = 0; j < WIDTH; j++){
+            for (int j = 0; j < WIDTH; j++) {
                 Tile newTile = new Tile();
                 newTile.SetValue("0");
                 current.add(newTile);
@@ -37,14 +37,15 @@ public class TwentyFortyEightBoard extends Board implements IInputHandler{
         System.out.println("????/");
         Tile newTile = new Tile();
         newTile.SetValue(randomStart.toString());
-        //Place First Tile
+        // Place First Tile
         this.PlaceTile(newTile, randomHeight, randomWidth);
-        //Check If there is tile
+        // Check If there is tile
     }
-    //Prints out current board
-    public List<List<Tile>> getBoard(){
-        for (int i = 0; i < HEIGHT; i++){
-            for (int j = 0; j < WIDTH; j++){
+
+    // Prints out current board
+    public List<List<Tile>> getBoard() {
+        for (int i = 0; i < HEIGHT; i++) {
+            for (int j = 0; j < WIDTH; j++) {
                 System.out.print(this.currentBoard.get(i).get(j).GetValue() + " ");
             }
             System.out.println(" ");
@@ -52,18 +53,20 @@ public class TwentyFortyEightBoard extends Board implements IInputHandler{
         return this.currentBoard;
     }
 
-    public void addRandomTile(){
-        // TODO: can be called for whenever an action is done, should be checking if a tile
-        // currently has a value or number on it. IDK the algo for it but if it does have 
-        // a value just  keep calling random Indexes
+    public void addRandomTile() {
+        // TODO: can be called for whenever an action is done, should be checking if a
+        // tile
+        // currently has a value or number on it. IDK the algo for it but if it does
+        // have
+        // a value just keep calling random Indexes
         boolean test = true;
-        while (test){
+        while (test) {
             Random random = new Random();
             Integer randomHeight = random.nextInt(4);
             Integer randomWidth = random.nextInt(4);
-            Integer randomStart = random.nextInt(1,2) * 2;
+            Integer randomStart = random.nextInt(1, 2) * 2;
             System.out.println(this.currentBoard.get(randomHeight).get(randomWidth).GetValue());
-            if (this.currentBoard.get(randomHeight).get(randomWidth).GetValue().equals("0")){
+            if (this.currentBoard.get(randomHeight).get(randomWidth).GetValue().equals("0")) {
                 System.out.println("testing");
                 Tile newTile = new Tile();
                 newTile.SetValue(randomStart.toString());
@@ -73,39 +76,11 @@ public class TwentyFortyEightBoard extends Board implements IInputHandler{
         }
     }
 
-    public void isGameOver(){
-        // TODO: called after every move, will check if there are any more moves that can be done
-        // If not call isGameOver in every iteration. If it returns true then it is Game Over
-    }
-    
-    //Algorithms for how the game should be run
-    @Override
-    public String GetInput() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'GetInput'");
-    }
-    @Override
-    public void Left() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'Left'");
-    }
-    @Override
-    public void Right() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'Right'");
-    }
-    @Override
-    public void Up() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'Up'");
-    }
-    @Override
-    public void Down() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'Down'");
+    public void isGameOver() {
+        // TODO: called after every move, will check if there are any more moves that
+        // can be done
+        // If not call isGameOver in every iteration. If it returns true then it is Game
+        // Over
     }
 
-
-
-    
 }
