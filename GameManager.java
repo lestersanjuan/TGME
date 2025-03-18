2ed6ae382ee256021bf0090aef6ae072b0e88d3b
Sean Bochaton <70482790+sbochato@users.noreply.github.com>
2025-03-18 01:53:56 -0700

    Update GameManager.java w comments

--- GameManager.java
+++ GameManager.java
@@ -3,70 +3,45 @@ import java.util.List;
 import java.util.Map;
 
 public abstract class GameManager {
-	public enum GameState {
-		NotStarted,
-		InProgress,
-		Ended,
-	}
+    
+    public enum GameState { NotStarted, InProgress, Ended } // Game states
+    public enum Result { Unset, Win, Lose } // Game results
 
-	public enum Result {
-		Unset,
-		Win,
-		Lose,
-	}
+    protected GameState status;
+    protected Result result;
+    protected GameEngine currentGameEngine;
+    protected Map<Integer, User> users;
+    protected IInputHandler inputHandler;
+    protected Map<Integer, GameEngine> games;
+    protected Map<Integer, Map<Integer, Integer>> leaderboard; // GameId -> (UserId -> Score)
 
-	protected GameState status;
-	protected Result result;
-	protected GameEngine currentGameEngine;
-	protected Map<Integer, User> users;
-	protected IInputHandler inputHandler;
-	protected Map<Integer, GameEngine> games;
-	protected Map<Integer, Map<Integer, Integer>> leaderboard; //GameId: UserId:Score
-
-	public GameManager() {
-		this.status = GameState.NotStarted;
-		this.result = Result.Unset;
-		this.users = new HashMap<Integer, User>();
-		this.games = new HashMap<Integer, GameEngine>();
-		this.leaderboard = new HashMap<Integer, Map<Integer, Integer>>();
-	}
-	
-	abstract void AddUser(String name, Integer id, List<String> data);
-	
-	public User GetUser(Integer id) {
-		return this.users.get(id);
-	}
-	
-	public void UpdateUser(User changedUser) {
-		this.users.put(changedUser.userId, changedUser);
-	}
-	
-	public void SetGameEngine(GameEngine gameEngine) {
-		this.currentGameEngine = gameEngine;
-	}
-	
-	//
-	public Board GetBoardState() {
-		return this.currentGameEngine.GetBoardState();
-	}
-	
-	public Result GetResult() {
-		return this.result;
-	}
-	
-	public GameState GetStatus() {
-		return this.status;
-	}
-	
-	public String GetInput() { //May not be used if a gui is being used
-		return this.inputHandler.GetInput();
-	}
-
-	public GameEngine GetGameEngine(Integer gameId) {
-		return this.games.get(gameId);
-	}
-	
-	public void SetNewScore(Integer gameId, Integer userId, Integer score) {
-		this.leaderboard.get(gameId).put(userId, score);
-	}
+    public GameManager() {
+        this.status = GameState.NotStarted;
+        this.result = Result.Unset;
+        this.users = new HashMap<>();
+        this.games = new HashMap<>();
+        this.leaderboard = new HashMap<>();
+    }
+    
+    abstract void AddUser(String name, Integer id, List<String> data); // Add a user
+    
+    public User GetUser(Integer id) { return this.users.get(id); } // Retrieve a user
+    
+    public void UpdateUser(User changedUser) { this.users.put(changedUser.userId, changedUser); } // Update user info
+    
+    public void SetGameEngine(GameEngine gameEngine) { this.currentGameEngine = gameEngine; } // Set current game engine
+    
+    public Board GetBoardState() { return this.currentGameEngine.GetBoardState(); } // Get current board state
+    
+    public Result GetResult() { return this.result; } // Get game result
+    
+    public GameState GetStatus() { return this.status; } // Get game status
+    
+    public String GetInput() { return this.inputHandler.GetInput(); } // Get user input
+    
+    public GameEngine GetGameEngine(Integer gameId) { return this.games.get(gameId); } // Get a game engine by ID
+    
+    public void SetNewScore(Integer gameId, Integer userId, Integer score) { 
+        this.leaderboard.get(gameId).put(userId, score); // Update leaderboard
+    }
 }
