ae229d895fa6b98db186e1f503e94be87b304cc9
Sean Bochaton <70482790+sbochato@users.noreply.github.com>
2025-03-18 01:54:56 -0700

    Update HomeScreen.java

--- HomeScreen.java
+++ HomeScreen.java
@@ -1,14 +1,14 @@
 import java.util.Map;
 
 public class HomeScreen {
-	private Map<Integer, GameEngine> games;
-	private Map<Integer, Map<Integer, Integer>> leaderboard; //GameId: UserId:Score
+    private Map<Integer, GameEngine> games; // Game ID -> GameEngine
+    private Map<Integer, Map<Integer, Integer>> leaderboard; // Game ID -> (User ID -> Score)
 
-	public GameEngine GetGameEngine(Integer gameId) {
-		return this.games.get(gameId);
-	}
-	
-	public void SetNewScore(Integer gameId, Integer userId, Integer score) {
-		this.leaderboard.get(gameId).put(userId, score);
-	}
+    public GameEngine GetGameEngine(Integer gameId) { 
+        return this.games.get(gameId); // Retrieve a game engine by ID
+    }
+
+    public void SetNewScore(Integer gameId, Integer userId, Integer score) { 
+        this.leaderboard.get(gameId).put(userId, score); // Update leaderboard score
+    }
 }
