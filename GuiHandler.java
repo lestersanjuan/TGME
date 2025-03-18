8c2e80875eba636fd197075ac46a93dcd803704f
Sean Bochaton <70482790+sbochato@users.noreply.github.com>
2025-03-18 01:54:28 -0700

    Update GuiHandler.java

--- GuiHandler.java
+++ GuiHandler.java
@@ -1,12 +1,11 @@
-
 public abstract class GuiHandler {
-	protected GameEngine gameEngine;
-	
-	abstract void UpdateUI(Board boardState);
-	
-	abstract void AffectGameEngine(String command);
-	
-	public void SetGameEngine(GameEngine gameEngine) {
-		this.gameEngine = gameEngine;
-	}
+    protected GameEngine gameEngine; // Reference to the game engine
+
+    abstract void UpdateUI(Board boardState); // Update UI based on board state
+
+    abstract void AffectGameEngine(String command); // Process user commands affecting the game
+
+    public void SetGameEngine(GameEngine gameEngine) { 
+        this.gameEngine = gameEngine; // Set the game engine
+    }
 }
